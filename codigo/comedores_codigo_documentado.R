9ef407999426b4d978158c5bf6b24a6f27de84fd
SIGEHgo <sigeh@hidalgo.gob.mx>
2025-11-21 09:24:58 -0600

    Se actualizo algunos detallitos para una version full para el laboratorio

--- codigo/comedores_codigo_documentado.R
+++ codigo/comedores_codigo_documentado.R
@@ -5,6 +5,7 @@ library(foreign)
 library(readxl)
 library(dplyr)
 library(raster)
+library(leafem)
 #Base previo a indice
 base_previo_indice=read_sf("output/base_previo_a_indice.shp")
 #Cobertura de Comedores
@@ -20,7 +21,8 @@ base_indice$benef[base_indice$benef|>is.nan()]=0
 base_indice$prc_at_cmd[base_indice$prc_at_cmd|>is.nan()]=0
 base_indice$prc_at_cmd[base_indice$prc_at_cmd|>is.na()]=0
 #pesos
-municipios=read_sf("../../../../../Reutilizables/Cartografia/municipiosjair.shp")
+municipios = sf::read_sf("../../Importantes_documentos_usar/Municipios/municipiosjair.shp")
+#municipios=read_sf("../../../../../Reutilizables/Cartografia/municipiosjair.shp")
 pesos=read_excel("output/ponderaciones.xlsx")
 pesos$Rank=(max(pesos$Rank)+1-pesos$Rank)/sum(max(pesos$Rank)+1-pesos$Rank)
 
@@ -31,9 +33,7 @@ library(sf)
 indice=function(transformacion,datos,pesos=pesos_f,interpretacion=c(rep(1,14),-1)){
   switch (substr(transformacion,1,1),
           "z" = {
-            datos= datos=lapply(datos,function(x){
-              rank(x)/length(x)
-            })|>as.data.frame()#scale(datos)
+            datos= scale(datos)
           },
           "l" = {
             datos=log(datos+1)
@@ -52,7 +52,8 @@ indice=function(transformacion,datos,pesos=pesos_f,interpretacion=c(rep(1,14),-1
   )
   return(as.matrix(datos)%*%(pesos*interpretacion))
 }
-indice(transformacion = "z",datos=base_indice[,3:17]|>st_drop_geometry())|>hist(main='Z-score')
+his
+indice(transformacion = "z",datos=base_indice[,3:17]|>st_drop_geometry())|>hist(main='Z-score',breaks=100)
 indice(transformacion = "l",datos=base_indice[,3:17]|>st_drop_geometry())|>hist(main='Log')
 indice(transformacion = "m",datos=base_indice[,3:17]|>st_drop_geometry())|>hist(main='Median/IQR')
 indice(transformacion = "r",datos=base_indice[,3:17]|>st_drop_geometry())|>hist(main='Ranking')
@@ -74,22 +75,52 @@ library(leaflet)
 library(reshape2)
 library(htmlwidgets)
 library(leaflet.extras)
+library(htmltools)
+
+info.box <- HTML(paste0(
+  HTML(
+    '<div class="modal fade" id="infobox" role="dialog"><div class="modal-dialog"><!-- Modal content--><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button>'
+  ),
+  
+  # Header / Title
+  HTML(
+    '</div><div class="modal-body">'
+  ),
+  
+  # Body
+  HTML('<h4>Información Adicional:</h4>
+<p> Usando datos del  <a href="https://www.inegi.org.mx" target="_blank">INEGI</a> y del <a href="https://www.coneval.org.mx" target="_blank">CONEVAL</a> a nivel AGEB (Áreas Geoestadísticas Básicas) sobre población y carencias por acceso a la alimentación, además de información georreferenciada de comedores comunitarios, se estimó la cobertura y población impactada a nivel AGEB. Esto permitió identificar cuáles comedores realmente atienden a la población que les corresponde, cuáles incluso la superan y, del mismo modo, cuáles no alcanzan a cubrir a la población que deberían atender. </p>
+<p> A partir de esa cobertura y de variables sociodemográficas adicionales (población de adultos mayores, personas con discapacidad, población infante) así como de variables geográficas y de accesibilidad (Accesibilidad a carretera pavimentada, índice de rezago social) se estandarizaron las variables mediante Z-scores y posteriormente se ponderaron para construir un ranking de prioridad alimentaria por AGEB. De esta manera nos permite identificar los AGEB con mayor prioridad de atención y su población objetivo.</p>'),
+  
+  # Closing divs
+  HTML('</div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button></div></div>')
+))
+
+
 pal_z_score = colorNumeric(palette = c("gray","yellow","orange","red"), domain = indices_pesos$z_score)
 pal_log = colorNumeric(palette = c("gray","gray","yellow", "yellow","orange","red", "red"), domain = indices_pesos$log)
 pal_median_IQR = colorNumeric(palette = c("gray","gray","gray","yellow","yellow", "yellow","yellow","yellow","orange","orange","orange","red", "red"), domain = indices_pesos$median_IQR)
 pal_ranking = colorNumeric(palette = c("gray","gray","yellow", "yellow","orange","red", "red"), domain = indices_pesos$ranking)
 
+quantile(x = indices_pesos$z_score, c(0.1,0.35,0.7))
+
+pal_z_score = colorQuantile(
+  palette = c("gray", "yellow", "orange", "red"),
+  domain  = indices_pesos$z_score,
+  n = 4,
+  na.color = "green"
+)
+
 mapa <- leaflet() |>
   # Agregar capa de fondo
-  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 1),group = "Basemap 100%") |>
-  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 0.5),group = "Basemap 50%") |>
-  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 0.25),group = "Basemap 25%") |>
+  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 1),group = "100%") |>
+  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 0.5),group = "50%") |>
+  addProviderTiles("OpenStreetMap", options = providerTileOptions(opacity = 0.25),group = "25%") |>
   # Agregar los polígonos de A
-  addPolygons(data=municipios,group = "municipios",fillColor = "grey",fillOpacity = 0.1,color = "white",stroke = 2,dashArray = "-",label = municipios$NOM_MUN) |> 
+  addPolygons(data = municipios, group = "municipios", fillColor = "grey", fillOpacity = 0.1, color = "white", stroke = 2, dashArray = "-",label = municipios$NOM_MUN) |> 
   # addPolygons(data = indices_pesos|>st_transform(crs(comedores))|>as("Spatial"),fillColor= 
   #               pal_log(indices_pesos$log)
   #             , color = pal_log(indices_pesos$log), fillOpacity = 0.6, 
-  #             popup = ~paste("Municipio:", indices_pesos$mun,"<br> Indice: ",indices_pesos$log|>round(2),
   #                            "<br> Poblacion: ",indices_pesos$POB1,
   #                            "<br> Carencia Alimentaria: ",indices_pesos$cr_p|>round(2)," %",
   #                            "<br> Atencion por comedores: ",indices_pesos$prc_at_cmd|>round(2)," %",
@@ -138,10 +169,10 @@ mapa <- leaflet() |>
               ,
               group = "Índice (Ranking)") |>
   addLayersControl(
-    baseGroups = c("Basemap 100%","Basemap 50%","Basemap 25%"),
-    overlayGroups = c("Índice (Ranking)"
+    baseGroups = c("100%","50%","25%"),
+    overlayGroups = c("Índice (Ranking)",
                       #,"Log","median_IQR","ranking","Comedores"
-                      ),
+                      "Comedores"),
     options = layersControlOptions(collapsed = FALSE)
   )|>
   
@@ -189,15 +220,42 @@ mapa <- leaflet() |>
         firstSwatch.style.border = '2px solid #0056B3'; // A darker, strong blue
                                                         // You can adjust '2px' for thickness
       }
+      
+      
+      var map = this;
+
+      // 1) Insertar un mensaje encima del control de capas
+      var layersControl = document.getElementsByClassName('leaflet-control-layers-list')[0];  // Devuelve un HTMLCollection con todos los nodos que tienen esa clase.
+      var instrEmpty = document.createElement('div');   // Crea un <div> vacío en memoria.
+      instrEmpty.style.height = '10px'; // Para dejar un margen visual.
+      instrEmpty.innerHTML = ' ';  // Ponemos un espacio como contenido, para asegurar que el <div> no colapse totalmente.
+      var instr = document.createElement('span'); // Línea que contendrá el mensaje.
+      instr.style.fontSize = '12px';  
+      instr.innerHTML = 'Transparencia del mapa';
+      layersControl.insertBefore(instrEmpty, layersControl.firstChild);  // Inserta newNode justo antes de referenceNode.
+      layersControl.insertBefore(instr, layersControl.firstChild);
+      
     }"
   ) |> 
-  addLogo(img ="https://raw.githubusercontent.com/JairEsc/Gob/main/Otros_archivos/imagenes/fondo_transparent.png",src = "remote",position ="bottomleft" ,width = "417px",height = "auto",offset.y = "0"  )
+  addLogo(img ="img/fondo_transparent.png",  position ="bottomleft" , width = "350", height = "175", offset.y = "0") |> 
+  addBootstrapDependency() %>% # Add Bootstrap to be able to use a modal
+  addEasyButton(easyButton(
+    icon = "fa-info-circle", title = "Map Information",
+    onClick = JS("function(btn, map){ $('#infobox').modal('show'); }")
+  )) %>% # Trigger the infobox
+  htmlwidgets::appendContent(info.box)
 
 
 mapa
+
+### Falta la paleta de colores
+
+
+
 library(htmlwidgets)
 library(leafem)
 #saveWidget(mapa, "output/mapas_web/leaflet_indices.html",selfcontained = T)
 #install.packages("htmlwidgets")
 htmlwidgets::saveWidget(widget = mapa, file = "tmp.html", selfcontained = TRUE) 
 file.rename("tmp.html", "output/mapas_web/leaflet_indices.html")
+
